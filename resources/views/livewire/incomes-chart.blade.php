203582ecb2ee88aeafc1605402b5e404ef97d2cf
arieldev <arielferaguirre.2001@gmail.com>
2024-01-26 16:04:03 -0500

    updated incomes chart

--- resources/views/livewire/incomes-chart.blade.php
+++ resources/views/livewire/incomes-chart.blade.php
@@ -4,41 +4,39 @@
         <canvas wire:ignore id="incomesChart"></canvas>
 
     </div>
-    @assets
-        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    @endassets
-    @script
-        <script>
-            const ctx = document.getElementById('incomesChart');
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-            const chart = new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    datasets: [{
-                        data: $wire.incomes,
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
+    <script>
+        const ctx = document.getElementById('incomesChart');
+
+        const chart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                datasets: [{
+                    data: @json($incomes),
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
                     }
                 }
-            });
-
-            $wire.on('refreshChart', () => {
-                chart.data.datasets[0].data = $wire.incomes;
+            }
+        });
+        document.addEventListener('livewire:init', () => {
+            Livewire.on('refreshChart', () => {
+                chart.data.datasets[0].data = @this.incomes;
                 chart.update();
             });
-        </script>
-    @endscript
+        });
+    </script>
 
 </div>
