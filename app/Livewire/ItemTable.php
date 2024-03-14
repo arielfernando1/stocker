c1d9388a5f1c8c6b02864b40e733dc8a243f5081
arieldev <arielferaguirre.2001@gmail.com>
2024-03-14 14:07:34 -0500

    improves

--- app/Livewire/ItemTable.php
+++ app/Livewire/ItemTable.php
@@ -2,9 +2,12 @@
 
 namespace App\Livewire;
 
+use App\Models\Category;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use App\Models\Item;
+use Illuminate\Database\Eloquent\Builder;
+use Rappasoft\LaravelLivewireTables\Views\Filters\SelectFilter;
 
 class ItemTable extends DataTableComponent
 {
@@ -40,6 +43,53 @@ class ItemTable extends DataTableComponent
                 ->sortable(),
             Column::make("Descripcion", "description")
                 ->sortable()
+        ];
+    }
+    public function filters(): array
+    {
+        return [
+            SelectFilter::make('Tipo')
+                ->options([
+                    '' => 'Todos',
+                    '1' => 'Producto',
+                    '2' => 'Servicio'
+                ])->filter(
+                    function (Builder $builder, string $value) {
+                        if ($value == '') {
+                            return $builder;
+                        } else if ($value == '1') {
+                            return $builder->whereNotNull('stock');
+                        } else if ($value == '2') {
+                            return $builder->whereNull('stock');
+                        }
+                    }
+                ),
+            SelectFilter::make('Categoria')
+                ->options(
+                    Category::pluck('name', 'id')->toArray()
+                )
+                ->filter(
+                    function (Builder $builder, $value) {
+                        return $builder->where('category_id', $value);
+                    }
+                ),
+            // no inventory filter (stock = 0)
+            SelectFilter::make('Stock')
+                ->options([
+                    '' => 'Todos',
+                    '1' => 'Con Stock',
+                    '2' => 'Sin Stock'
+                ])->filter(
+                    function (Builder $builder, string $value) {
+                        if ($value == '') {
+                            return $builder;
+                        } else if ($value == '1') {
+                            return $builder->where('stock', '>', 0);
+                        } else if ($value == '2') {
+                            return $builder->where('stock', 0);
+                        }
+                    }
+                )
         ];
     }
 }
